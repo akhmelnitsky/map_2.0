e9ee85a5ffe30d5df0dd80e7613ee814117c084d
Ubuntu <ubuntu@ip-172-31-44-164.us-west-2.compute.internal>
2015-09-26 05:51:05 +0000

    Removed dublicate

--- map-app/lib/page-map/app.js
+++
@@ -1,8 +0,0 @@
-// page-map/app.js
-var express = require('express');
-var app = module.exports = express();
-//app.use(express.static())
-
-app.get('/map', function(req,res) {
-	res.sendFile(__dirname + '/page-map.html');
-});
